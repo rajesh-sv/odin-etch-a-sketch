50ce5687a4f595cfb6a2cf13b812626e03bf4e52
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2023-05-27 22:06:51 +0530

    Add sketch area clear, delete and create logic in js

--- script.js
+++ script.js
@@ -3,13 +3,60 @@ const DEFAULT_GRID_SIZE = 16;
 
 let cellColor = DEFAULT_CELL_COLOR;
 let gridSize = DEFAULT_GRID_SIZE;
+let mouseDown = false;
 
+const gridDiv = document.querySelector('.grid');
 const colorPicker = document.querySelector('#color-picker');
 const eraserButton = document.querySelector('#eraser-button');
 const clearButton = document.querySelector('#clear-button');
 const gridSizeButton = document.querySelector('#grid-size-button');
 
-gridSizeButton.textContent = `Grid Size: ${gridSize}`;
+window.onload = () => {
+  colorPicker.value = cellColor;
+  gridSizeButton.textContent = `Grid Size: ${gridSize}`;
+  createGrid();
+}
+
+function clearGrid() {
+  const gridCells = Array.from(gridDiv.childNodes);
+  gridCells.forEach((cell) => {
+    cell.style.backgroundColor = '#ffffff';
+  });
+}
+
+function changeCellColor(e) {
+  e.target.style.backgroundColor = cellColor;
+  e.stopPropagation();
+}
+
+function deleteGrid() {
+  gridDiv.textContent = '';
+}
+
+function createGrid() {
+  const cellWidth = `${512/gridSize}px`;
+  const cellHeight = cellWidth;
+  const numCells = gridSize ** 2;
+  for (i=0; i<numCells; ++i)
+    gridDiv.appendChild(createCell(cellWidth, cellHeight))
+}
+
+function createCell(cellWidth, cellHeight) {
+  const div = document.createElement('div');
+  div.style.width = cellWidth;
+  div.style.height = cellHeight;
+  div.classList.add('grid-cell');
+  div.addEventListener('mousedown', (e) => {
+    mouseDown = true;
+    changeCellColor(e);
+  });
+  div.addEventListener('mouseup', () => mouseDown = false);
+  div.addEventListener('mouseover', (e) => {
+    if(mouseDown)
+      changeCellColor(e); 
+  });
+  return div;
+}
 
 colorPicker.addEventListener('input', (e) => {
   eraserButton.classList.remove('eraser-button-on');
@@ -26,6 +73,7 @@ eraserButton.addEventListener('click', () => {
 clearButton.addEventListener('click', () => {
   eraserButton.classList.remove('eraser-button-on');
   clearGrid();
+  cellColor = colorPicker.value;
 });
 
 gridSizeButton.addEventListener('click', () => {
@@ -39,6 +87,7 @@ gridSizeButton.addEventListener('click', () => {
     deleteGrid();
     createGrid();
   }
+  cellColor = colorPicker.value;
 });
 
 function getValidGridSize() {
